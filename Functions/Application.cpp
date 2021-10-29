fa809ad5e08fa31f9e7fce1c4457f402d5e8efe8
Anis karam <anisTkaram@hotmail.com>
2021-10-29 13:19:03 +0300

    Simple Function
    1- A function created (declaration and definition).
    2- Calling that function in Main function.

---
+++ Functions/Application.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+void myFunction();
+
+int main()
+{
+	myFunction();
+	system("pause");
+}
+
+void myFunction()
+{
+	std::cout << "Hello and Welcome to my first functon!" << std::endl;
+}
