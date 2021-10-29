ab52666c407da80a85c9a3dcc4f1ab8e14799712
Anis karam <anisTkaram@hotmail.com>
2021-10-29 14:00:18 +0300

    Single/Double argument, default value
    1- Function that takes one argument.
    2- Function that takes three arguments.
    3- Default value for the function.

--- Functions/Application.cpp
+++ Functions/Application.cpp
@@ -1,14 +1,56 @@
 #include <iostream>
+using namespace std;
 
+// Simple function decleration
 void myFunction();
+
+// One Argument
+void introducingMySelf(string name);
+
+// Multiple Arguments
+// Default values, works from RIGHT to LEFT
+// ! WE CAN'T SET DEFAULT VALUE for middle argument or left argument before setting for the last one
+void introduceYourSelf(string name, int age = 0, double height = 0.0);
 
 int main()
 {
+	string n;
+	int a;
+	double h;
+
 	myFunction();
+	//introducingMySelf("anis");
+	//introduceYourSelf("George");
+
+	std::cout << "Enter your name: ";
+	std::cin >> n;
+	std::cout << "Enter your age: ";
+	std::cin >> a;
+	std::cout << "Enter your height: ";
+	std::cin >> h;
+
+	introduceYourSelf(n, a, h);
+
 	system("pause");
 }
 
-void myFunction()
+// Simple function defenition
+void myFunction()	
 {
 	std::cout << "Hello and Welcome to my first functon!" << std::endl;
+}
+
+// One Argument
+void introducingMySelf(string name)
+{
+	std::cout << "My name is " << name << " thank you!" << std::endl;
+}
+
+// Multiple Arguments
+void introduceYourSelf(string name, int age, double height)
+{
+	if (age != 0 && height != 0.0)
+		std::cout << "My name is " << name << ", " << age << " years old, and " << height << " height" << std::endl;
+	else
+		std::cout << "My name is " << name << std::endl;
 }
